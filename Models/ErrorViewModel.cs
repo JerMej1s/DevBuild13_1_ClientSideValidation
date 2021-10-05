c379ae20300c12f2283869ceb6542a7b9dcbeb3d
Jeremy Jones <JeremyJones@QuickenLoans.com>
2021-10-05 19:30:07 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab13_1_ClientSideValidation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
